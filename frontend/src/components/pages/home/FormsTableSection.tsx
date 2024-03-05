5fa6c588d9834c9a0319264386f0d3fe2c262c92
Andres Patino <apatinoc4@gmail.com>
2024-03-04 19:14:30 -0500

    fix: behavior on table after deletion

--- frontend/src/components/pages/home/FormsTableSection.tsx
+++ frontend/src/components/pages/home/FormsTableSection.tsx
@@ -1,18 +1,23 @@
-'use client'
 import Table from '@/components/Table'
 import useGetForms from '@/hooks/useGetForms'
 import {createColumnHelper, Row} from '@tanstack/react-table'
 import {Form, FormKeys} from '@/types/Forms'
 import Link from 'next/link'
-import {useMemo} from 'react'
-import {FaMagnifyingGlass} from 'react-icons/fa6'
-import {FaTrash} from 'react-icons/fa6'
+import {useMemo, useEffect, useState} from 'react'
+import {FaMagnifyingGlass, FaTrash} from 'react-icons/fa6'
 import useDeleteForm from '@/hooks/useDeleteForm'
 
 const FormsTableSection: React.FC = () => {
-    const {data, isLoading} = useGetForms()
+    const {data, isLoading, refetch} = useGetForms()
     const forms = useMemo(() => data?.automatic_response, [data])
     const {deleteForm} = useDeleteForm()
+    const [shouldRefetch, setShouldRefetch] = useState<boolean>(false)
+
+    const handleDeleteForm = async (id: string) => {
+        await deleteForm(id)
+        setShouldRefetch(true)
+    }
+
     const columnHelper = createColumnHelper<Form>()
     const columns = [
         columnHelper.accessor('automatic_response_id', {
@@ -26,29 +31,35 @@ const FormsTableSection: React.FC = () => {
         {
             id: 'actions',
             header: () => null,
-            cell: ({row}: {row: Row<Form>}) => {
-                return (
-                    <div className='flex flex-row gap-4'>
-                        <Link
-                            href={`/automatic-responses/${row.getValue('automatic_response_id')}`}
-                            target='_blank'>
-                            <FaMagnifyingGlass color='#455d7a' />
-                        </Link>
-                        <div className='cursor-pointer'>
-                            <FaTrash
-                                onClick={async () =>
-                                    deleteForm(
-                                        row.getValue('automatic_response_id')
-                                    )
-                                }
-                                color='#f95959'
-                            />
-                        </div>
+            cell: ({row}: {row: Row<Form>}) => (
+                <div className='flex flex-row gap-4'>
+                    <Link
+                        href={`/automatic-responses/${row.getValue('automatic_response_id')}`}
+                        target='_blank'>
+                        <FaMagnifyingGlass color='#455d7a' />
+                    </Link>
+                    <div className='cursor-pointer'>
+                        <FaTrash
+                            onClick={() =>
+                                handleDeleteForm(
+                                    row.getValue('automatic_response_id')
+                                )
+                            }
+                            color='#f95959'
+                        />
                     </div>
-                )
-            },
+                </div>
+            ),
         },
     ]
+
+    useEffect(() => {
+        if (shouldRefetch) {
+            refetch()
+            setShouldRefetch(false)
+        }
+    }, [shouldRefetch])
+
     return (
         <section className='w-full'>
             <h2 className='mb-4 text-3xl font-extrabold text-slate-800'>
