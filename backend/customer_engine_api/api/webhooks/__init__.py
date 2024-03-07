d897c93597e3e0c15c1ed2eb6b853d2ae31b0a9a
Tomperez98 <tomasperezalvarez@gmail.com>
2024-03-07 12:39:23 -0500

    Upgrade dependencies

--- backend/customer_engine_api/api/webhooks/__init__.py
+++ backend/customer_engine_api/api/webhooks/__init__.py
@@ -7,6 +7,8 @@ from fastapi import APIRouter, Request, Response, status
 from fastapi.exceptions import HTTPException
 
 from customer_engine_api.config import resources
+from customer_engine_api.core import automatic_responses, whatsapp
+from customer_engine_api.core.whatsapp import core as whatsapp_core
 from customer_engine_api.logging import logger
 
 router = APIRouter(prefix="/webhooks", tags=["webhooks"])
@@ -14,9 +16,6 @@ router = APIRouter(prefix="/webhooks", tags=["webhooks"])
 
 @router.get("/whatsapp/{org_code}")
 async def suscribe_whatsapp_webhooks(org_code: str, req: Request) -> Response:  # noqa: D103
-    from customer_engine_api.core import whatsapp
-    from customer_engine_api.core.whatsapp import core
-
     verify_token = req.query_params["hub.verify_token"]
     with resources.db_engine.begin() as conn:
         response, events = await lego_workflows.run_and_collect_events(
@@ -24,7 +23,7 @@ async def suscribe_whatsapp_webhooks(org_code: str, req: Request) -> Response:  
         )
     await lego_workflows.publish_events(events=events)
     stored_tokens = response.whatsapp_token
-    if not core.check_same_hashed(
+    if not whatsapp_core.check_same_hashed(
         hashed=stored_tokens.user_token, string=verify_token, algo="sha256"
     ):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
@@ -34,5 +33,18 @@ async def suscribe_whatsapp_webhooks(org_code: str, req: Request) -> Response:  
 
 @router.post("/whatsapp/{org_code}")
 async def whatsapp_webhooks(org_code: str, req: Request) -> None:  # noqa: ARG001, D103
-    logger.debug(await req.json())
-    raise NotImplementedError
+    payload = await req.json()
+    with resources.db_engine.begin() as conn:
+        most_similar_response, events = await lego_workflows.run_and_collect_events(
+            cmd=automatic_responses.cmd.search_by_prompt.Command(
+                org_code=resources.default_org,
+                prompt=payload["entry"][0]["changes"][0]["value"]["messages"][0][
+                    "text"
+                ]["body"],
+                sql_conn=conn,
+                cohere_client=resources.clients.cohere,
+                qdrant_client=resources.clients.qdrant,
+            )
+        )
+    await lego_workflows.publish_events(events=events)
+    logger.info(most_similar_response)
