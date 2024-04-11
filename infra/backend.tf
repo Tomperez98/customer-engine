ad6722b47c26d1560da2ea1b83d4997d8773dd87
Tomperez98 <tomasperezalvarez@gmail.com>
2024-04-11 18:56:54 -0500

    Infrastructure settings

---
+++ infra/backend.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    bucket         = "tomp-aws-terraform-states"
+    key            = "customer-engine/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "tomp-aws-terraform-states"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.65"
+    }
+  }
+
+  required_version = ">= 1.3.7"
+}
+
+
+provider "aws" {
+  profile = "tomp-aws"
+  region  = "us-east-1"
+  default_tags {
+    tags = {
+      Terraform      = true
+      CustomerEngine = true
+    }
+
+  }
+}
